d36d9841818edf8062ba055dbfdfdc6e7849b9f2
Ariel Solomon <ariel.solomon@gmail.com>
2023-06-26 14:47:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print('\nMade a change in the code!!!')
+    print('\nMade aaa change in the code!!!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
